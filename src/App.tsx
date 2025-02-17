56d06c0cd64c34223183dd939bf05695289ae973
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-02-18 01:06:18 +0900

    add prop based style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { css } from '@linaria/core'
+import { styled } from '@linaria/react'
 import { font } from './font'
 
 export const globals = css`
@@ -18,11 +19,19 @@ function App() {
     font-size: ${font.large};
   `
 
+  type ParagraphProps = {
+    fontSize?: 'small' | 'medium' | 'large'
+  }
+  const Paragraph = styled.p`
+    font-size: ${({ fontSize = 'small' }: ParagraphProps) => font[fontSize]};
+  `
+
   return (
     <>
       <div className="container">
         <h1 className={title}>Linaria Playground</h1>
         <h2 className={subtitle}>Sub Title</h2>
+        <Paragraph fontSize="medium">Text Text Text</Paragraph>
       </div>
     </>
   )
