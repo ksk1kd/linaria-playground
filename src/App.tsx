f002269c4e2d68f469689d2567c72190dfb8521b
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-02-18 00:47:00 +0900

    add variable style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { css } from '@linaria/core'
+import { font } from './font'
 
 function App() {
   const title = css`
     font-size: 4rem;
   `
 
+  const subtitle = css`
+    font-size: ${font.large};
+  `
+
   return (
     <>
       <div className="container">
         <h1 className={title}>Linaria Playground</h1>
+        <h2 className={subtitle}>Sub Title</h2>
       </div>
     </>
   )
---
+++ src/font.ts
@@ -0,0 +1,5 @@
+export const font = {
+  small: '1rem',
+  medium: '2rem',
+  large: '3rem',
+}
